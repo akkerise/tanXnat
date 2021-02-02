e40dac419351af8875229a0be67a69a2ba0cfbda
thanhna <thanhna@meeyland.com>
2021-02-02 18:48:27 +0700

    update lib/validator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const fp = require('./lib/functional')
 const util = require('./lib/util')
 const redis = require('./lib/redis')
+const common = require('./lib/common')
+const validator = require('./lib/validator')
 module.exports = {
   fp,
   util,
-  redis
+  redis,
+  common,
+  validator
 }
--- src/lib/validator.js
+++ src/lib/validator.js
@@ -1,5 +1,11 @@
-const email = (val) => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(val).toLowerCase())
-const phone = (val) => /^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/.test(String(val).toLowerCase())
+const email = (val) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(val).toLowerCase());
+}
+const phone = (val) => {
+  const re = /^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/;
+  return re.test(String(val).toLowerCase());
+}
 
 module.exports = {
   email, phone
