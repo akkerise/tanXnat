ab8f3506acbdad6e52204a43c00289eb7a2345c0
thanhna <thanhna@meeyland.com>
2021-02-03 14:18:12 +0700

    update lib/core

---
+++ src/lib/core.js
@@ -0,0 +1,25 @@
+const distinct = arrs => {
+  if (arrs === 'undefined' || !(arrs instanceof Array)) return
+  let found = new Set()
+  return this.filter(element => {
+    if (found.has(selector(element))) return false
+    found.add(selector(element))
+    return true
+  })
+}
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+const uniq = (raws, key) => [...new Map(raws.map(item => [item[key], item])).values()]
+
+const compose = (...fns) => fns.reduce((f, g) => (x) => f(g(x)))
+
+const pipe = (...fns) => fns.reduce((f, g) => (x) => g(f(x)));
+
+module.exports = {
+  distinct,
+  compose,
+  delay,
+  pipe,
+  uniq
+} 
