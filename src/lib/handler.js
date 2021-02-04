c044d0228e35c7cce8d7bc59d9191af55bb0b006
thanhna <thanhna@meeyland.com>
2021-02-04 18:15:39 +0700

    update lib/handler

--- src/lib/handler.js
+++ src/lib/handler.js
@@ -4,24 +4,16 @@ const RESPONSES = {
   400: {status: false, message: 'Bad request'},
   404: {status: false, message: 'Not found'},
   500: {status: false, message: 'Server error'},
-  555: {status: false, message: 'Something went wrong'},
+  666: {status: false, message: 'Something went wrong'},
 }
 
 const isSuccess = response => response.error && response.error.status === true
 
 const getData = response => response.data || null
 
-const getError = code => code ? RESPONSES[code] : RESPONSES[555]
+const getError = code => code ? RESPONSES[code] : RESPONSES[666]
 
 const get = by => arr => by(arr)
-
-const res = {
-  error: {
-    code: 200,
-    status: true
-  },
-  data: []
-}
 
 const getSuccess = get(isSuccess);
 
@@ -30,6 +22,16 @@ const data = get(getData)
 const error = get(getError)
 
 const getResponse = (response) => ({error: error(response), data: data(response)})
+
+/**
+const res = {
+  error: {
+    code: 200,
+    status: true
+  },
+  data: []
+}
+*/
 
 module.exports = {
   getResponse,
