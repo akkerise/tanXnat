bd1c123275acc214b01734d4a671e2cf33c977d5
thanhna <thanhna@meeyland.com>
2021-02-23 09:16:18 +0700

    update lib/test

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,18 +1,18 @@
-const cua = (x) => `${x} đã cưa`;
+// const cua = (x) => `${x} đã cưa`;
 
-const say = (x) => `${x} đã sấy`;
+// const say = (x) => `${x} đã sấy`;
 
-const bao = (x) => `${x} đã bào`;
+// const bao = (x) => `${x} đã bào`;
 
-const khoan = (x) => `${x} đã khoan`;
+// const khoan = (x) => `${x} đã khoan`;
 
-const cha = (x) => `${x} đã chà`;
+// const cha = (x) => `${x} đã chà`;
 
-const moc = (x) => `${x} đã gắn móc`;
+// const moc = (x) => `${x} đã gắn móc`;
 
-const compose = (...fns) => {
-  return fns.reduce((f, g) => (x) => f(g(x)));
-};
+// const compose = (...fns) => {
+//   return fns.reduce((f, g) => (x) => f(g(x)));
+// };
 
-const total = compose(moc, cha, khoan, bao, say, cua);
-console.log(total.toString());
+// const total = compose(moc, cha, khoan, bao, say, cua);
+// console.log(total.toString());
