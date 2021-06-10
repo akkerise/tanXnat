e20b35a2b28b9f343f76156cc58326856b0eca6d
Thanh Nguyễn Anh <thanhna@meeyland.com>
2021-06-11 00:56:52 +0700

    update lib/sample/pattern/curry.js

--- src/lib/sample/pattern/curry.js
+++ src/lib/sample/pattern/curry.js
@@ -1,11 +1,10 @@
 
-const sum = (a, b, c) => a + b + c;
+const sum = (a, b, c) => a + b + c
 const multiply = (a, b, c) => a * b * c;
 
 const curry = (fn) => {
   let totalArguments = fn.length;
-  let next = (argumentLength, rest) => {
-    console.log('argumentLength :>> ', argumentLength);
+  const next = (argumentLength, rest) => {
     return argumentLength > 0
       ? (...args) => next(argumentLength - args.length, [...rest, ...args])
       : fn(...rest)
@@ -14,9 +13,12 @@ const curry = (fn) => {
 };
 
 const curriedSum = curry(sum);
+const curriedMultiply = curry(multiply);
 
 console.log('curriedSum(4, 4, 2) :>> ', curriedSum(4, 4, 2)); // => 10
 console.log('curriedSum(4, 3, 3) :>> ', curriedSum(4, 3, 3)); // => 10
 console.log('curriedSum(3, 5, 2) :>> ', curriedSum(3, 5, 2)); // => 10
-console.log('curriedSum(5, 3)(2) :>> ', curriedSum(5, 3)(2)); // => 10
+console.log('curriedSum(5, 3) :>> ', curriedSum(5, 3)); // => 10
 console.log('curriedSum(5)(3)(2) :>> ', curriedSum(5)(3)(2)); // => 10
+console.log('curriedSum(5, 3)(2, 4, 8) :>> ', curriedSum(5, 3)(2, 4, 8));
+console.log('curriedMultiply(2,2,2) :>> ', curriedMultiply(2, 2, 2)); // => 8
