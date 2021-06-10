c4c1036d6f26266b20818e01adcb3f9bef5739c7
Thanh Nguyễn Anh <thanhna@meeyland.com>
2021-06-10 17:09:22 +0700

    update lib/sample/pattern/builder.js

---
+++ src/lib/sample/pattern/builder.js
@@ -0,0 +1,43 @@
+class Car {
+  constructor(engine, chassis, body) {
+    this.engine = engine;
+    this.chassis = chassis;
+    this.body = body;
+  }
+
+  toString() {
+    return JSON.stringify(this)
+  }
+}
+
+class CarBuilder {
+  addChassis(chassis) {
+    this.chassis = chassis;
+    return this;
+  }
+  addEngine(engine) {
+    this.engine = engine;
+    return this;
+  }
+  addBody(body) {
+    this.body = body;
+    return this;
+  }
+  build() {
+    return new Car(this.engine, this.chassis, this.body);
+  }
+}
+
+const car = new CarBuilder()
+  .addEngine('v12')
+  .addBody('KIA SOLUTION')
+  .addChassis('LUXURY')
+  .build();
+
+const car1 = new CarBuilder()
+  .addEngine('v10')
+  .addBody('KIA SOLUTION LUXURY')
+  .build();
+
+console.log('Car', car.toString())
+console.log('Car1', car1.toString())
